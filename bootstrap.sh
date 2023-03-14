956c064a6a55b0549f1cc1a548df5ffa3d988c88
ironman <ironman@Mark85>
2023-03-14 02:37:48 -0300

    version 1.2

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,57 +2,27 @@
 
 apt-get update
 
-# unzip is for composer
-apt-get install vim unzip  -y
-
 apt-get install apache2 -y
 
-##apt-get install mysql-client libmysqlclient-dev -y
+##apt-get install php 
 apt-get install libapache2-mod-php7.4 php7.4 php7.4-mysql php7.4-sqlite -y
 apt-get install php7.4-mbstring php7.4-curl php7.4-intl php7.4-gd php7.4-zip php7.4-bz2 -y
 apt-get install php7.4-dom php7.4-xml php7.4-soap -y
 apt-get install --reinstall ca-certificates -y
 
-# install the php xdebug extension (only for dev servers!)
-apt-get install php-xdebug -y
-
-# add xdebug settings to php.ini
-echo 'xdebug.remote_port=9000' >> /etc/php/7.4/apache2/php.ini
-echo 'xdebug.remote_enable=true' >> /etc/php/7.4/apache2/php.ini
-echo 'xdebug.remote_connect_back=true' >> /etc/php/7.4/apache2/php.ini
-echo 'xdebug.remote_autostart=on' >> /etc/php/7.4/apache2/php.ini
-echo 'xdebug.remote_host=' >> /etc/php/7.4/apache2/php.ini
-echo 'xdebug.max_nesting_level=1000' >> /etc/php/7.4/apache2/php.ini
-echo 'xdebug.idekey=PHPSTORM' >> /etc/php/7.4/apache2/php.ini
-
 # Enable apache mod_rewrite
 a2enmod rewrite
 a2enmod actions
 
-# Change AllowOverride from None to All (between line 170 and 174)
-sed -i '170,174 s/AllowOverride None/AllowOverride All/g' /etc/apache2/apache2.conf
+cp -rf /vagrant/apache2.conf /etc/apache2/
+
+cd /vagrant
+
+rm -f /vagrant/index.php
+
+wget https://raw.githubusercontent.com/marvenarg/demo-php/main/index.php --secure-protocol=TLSv1_2
+
+cp -rf /vagrant/index.php /var/www/html/
 
 # Start the webserver
 service apache2 restart
-
-# Install MySQL (optional)
-##apt-get install mysql-server -y
-
-# Change mysql root password
-##service mysql start
-##mysql -u root --password="" -e "update mysql.user set authentication_string=password(''), plugin='mysql_native_password' where user='root';"
-##mysql -u root --password="" -e "flush privileges;"
-
-# Install composer
-##cd ~
-##php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-##php composer-setup.php --install-dir=/usr/local/bin --filename=composer
-##php -r "unlink('composer-setup.php');"
-##composer self-update
-
-git clone https://github.com/marvenarg/demo-php.git
-
-# Create a symlink
-rm -rf /var/www
-mkdir /var/www
-ln -s /vagrant/ /var/www/html
